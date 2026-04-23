099aaac2b00f2c91198d0dd6dd9babd66bbcf174
sangitanandaninand <30070686@cc.peralta.edu>
2026-04-23 05:30:19 +0000

    Fixed the structure of Game.h

--- src/Game.h
+++ src/Game.h
@@ -2,12 +2,11 @@
 
 class Game {
 private:
-}
 int secretNumber; //Holds the random number to guess
-int tries; //Counts how much guesses the user has made which keeps track of the guesses.
+int tries; //Counts how much guesses the user has made.
 
 public:
-Game(); //Constructor
-void startGame(); {
+Game(); //Constructor declaration
+void startGame(); //Function declaration (no function body)
 
 };
